26aaaa3d0e7728e2ec67ee4f99d0b2ee5f1698de
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-02-06 18:54:37 +0100

    Added usage scenario
    Added a successful usage scenario, then an unsuccessful scenario.
    Gave some fluff with print statements to make it easier to read.
    Code handles exception thrown and prints to consonle.

--- CoffeeMaker/src/main/Main.java
+++ CoffeeMaker/src/main/Main.java
@@ -5,91 +5,126 @@ import model.exceptions.*;
 
 public class Main {
 
+    /*
+     * UBCx SoftwareConstruction2x - Module 1
+     *
+     * CoffeeMaker long form problem Solution
+     * create robust software by using exceptions
+     *
+     * Valid amounts are:
+     * beans 2.4<->2.6
+     * water > 14.75
+     *
+     */
+
+
     public static void main(String[] args) {
-        // TODO: complete the implementation of Main
+        // Use case scenario: first a successful usage scenario, then an unsuccessful scenario
 
 
-        // Successful Path! beans 2.4<->2.6, water >14.75
+        // Successful usage
+        CoffeeMaker cm = new CoffeeMaker();
+
+        try {
+
+            System.out.println("Brewing coffee");
+            cm.brew(2.5, 15);
+            System.out.println("Cups left: " + cm.getCupsRemaining() +
+            "\nTime since fresh brew: " + cm.getTimeSinceLastBrew() + "\n");
+
+            System.out.println("pouring a cup");
+            cm.pourCoffee();
+
+            System.out.println("taking a quick break");
+            cm.setTimeSinceLastBrew(10);
+
+            System.out.println("pouring some more cups");
+            cm.pourCoffee();
+            cm.pourCoffee();
+            cm.pourCoffee();
+            cm.setTimeSinceLastBrew(30);
+
+            System.out.println("Wonder how how coffee is left and how long it was since i brewed it" +
+                    "\nCups left: " + cm.getCupsRemaining() +
+                    "\nTime since fresh brew: " + cm.getTimeSinceLastBrew() + "\n");
+
+            System.out.println("refreshing the pot");
+            cm.brew(2.5, 20);
+            System.out.println("Cups left: " + cm.getCupsRemaining() +
+                    "\nTime since fresh brew: " + cm.getTimeSinceLastBrew() + "\n");
+
+        } catch (Exception e) {
+            System.out.println("not expected");
+        }
+
+
+        // Unsuccessful usage
         CoffeeMaker coffeeMaker = new CoffeeMaker();
 
         try {
-            coffeeMaker.brew(2.5, 15.0);
-
-        } catch (BeansAmountException e) {
-            e.printStackTrace();
-            e.getMessage();
-
+            coffeeMaker.brew(2.5, 13.0);
         } catch (WaterException e) {
-            e.printStackTrace();
-            e.getMessage();
+            System.out.println("expected, water");
+        } catch (BeansAmountException e) {
+            System.out.println("unexpected");
         }
 
-        coffeeMaker.setTimeSinceLastBrew(10);
+        try {
+            coffeeMaker.brew(2.1, 15);
+        } catch (BeansAmountException e) {
+            System.out.println("expected, beans");
+        } catch (WaterException e) {
+            System.out.println("unexpected");
+        }
+
+        try {
+            coffeeMaker.brew(2.5, 15);
+        } catch (BeansAmountException e) {
+            System.out.println("unexpected");
+        } catch (WaterException e) {
+            System.out.println("unexpected");
+        }
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                coffeeMaker.pourCoffee();
+            } catch (NoCupsRemainingException e) {
+                System.out.println("unexpected");
+            } catch (StaleCoffeeException e) {
+                System.out.println("unexpected");
+            }
+        }
+
+        try {
+            coffeeMaker.pourCoffee(); // 1 too many!
+        } catch (NoCupsRemainingException e) {
+            System.out.println("expected, no cups");
+        } catch (StaleCoffeeException e) {
+            System.out.println("unexpected");
+        }
+
+        try {
+            coffeeMaker.brew(2.5, 15);
+        } catch (BeansAmountException e) {
+            System.out.println("unexpected");
+        } catch (WaterException e) {
+            System.out.println("unexpected");        }
+
+        coffeeMaker.setTimeSinceLastBrew(61);
 
         try {
             coffeeMaker.pourCoffee();
-
         } catch (NoCupsRemainingException e) {
-            e.printStackTrace();
-            System.out.println("No cups remaining");
-            e.getMessage();
-
+            System.out.println("unexpected");
         } catch (StaleCoffeeException e) {
-            e.printStackTrace();
-            System.out.println("Coffee has gone stale");
-            e.getMessage();
+            System.out.println("expected, stale coffee");
         }
 
-
-        // Unsuccessful path!
-        CoffeeMaker cm2 = new CoffeeMaker();
-
-        try {
-            coffeeMaker.brew(1,5);
-        } catch (BeansAmountException e) {
-            e.printStackTrace();
-            e.getMessage();
-
-        } catch (WaterException e) {
-            e.printStackTrace();
-            e.getMessage();
-        }
-
-        try {
-            cm2.brew(2.5, 16.0);
-        } catch (BeansAmountException e) {
-            e.printStackTrace();
-            e.getMessage();
-
-        } catch (WaterException e) {
-            e.printStackTrace();
-            e.getMessage();
-        }
-
-        cm2.setTimeSinceLastBrew(50);
-        try {
-            cm2.pourCoffee();
-        } catch (NoCupsRemainingException e) {
-            e.printStackTrace();
-            e.getMessage();
-
-        } catch (StaleCoffeeException e) {
-            e.printStackTrace();
-            e.getMessage();
-        }
-
-        cm2.setTimeSinceLastBrew(70);
-        try {
-            cm2.pourCoffee();
-        } catch (NoCupsRemainingException e) {
-            e.printStackTrace();
-            e.getMessage();
-
-        } catch (StaleCoffeeException e) {
-            e.printStackTrace();
-            e.getMessage();
-        }
-
+        System.out.println();
+        System.out.println("Barista went home for the day" +
+        "\nCups Remaining: " + coffeeMaker.getCupsRemaining() +
+        "\nTime since fresh brew: " + coffeeMaker.getTimeSinceLastBrew());
 
     }
+
 }
